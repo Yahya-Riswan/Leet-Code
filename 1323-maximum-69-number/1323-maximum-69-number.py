59b94344bde1ef2d66fe897535074243de0c1dfd
Yahya Riswan Pk <pk.yahyariswan@gmail.com>
2026-01-09 09:54:45 +0530

    Time: 0 ms (100%), Space: 12.3 MB (82.86%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -4,15 +4,5 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        results = [num]
-        s_num = str(num)
-        for i in range(len(s_num)):
-            st = list(s_num)
-            if st[i] == "9":
-                st[i] = "6"
-            else:
-                st[i] = "9"
-            new_num = int("".join(st))
-            results.append(new_num)
-
-        return max(results)
+        num_str = str(num)
+        return int(num_str.replace('6', '9', 1))
